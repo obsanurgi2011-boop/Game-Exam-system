70965099a130681b84d592f3b9d3acf6e31756eb
obsanurgi2011-boop <obsanurgi2011@gmail.com>
2026-07-01 00:44:27 +0300

    Initialize Kivy app with screen manager and screens

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
+from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager
+from auth import UserAuth
+from screens import LoginScreen, DashboardScreen, ExamScreen
 
+class SM(ScreenManager):
+    pass
+
+class GoExam(App):
+    def build(self):
+        sm = SM()
+
+        sm.auth = UserAuth()
+
+        sm.add_widget(LoginScreen(name="login"))
+        sm.add_widget(DashboardScreen(name="dashboard"))
+        sm.add_widget(ExamScreen(name="exam"))
+
+        return sm
+
+GoExam().run()
