3eb450d2fcfdc0286a1328b5137126e8190c37f7
obsanurgi2011-boop <obsanurgi2011@gmail.com>
2026-07-01 01:39:48 +0300

    Refactor app structure and add Admin and Exams screens

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager
 from auth import UserAuth
-from screens import LoginScreen, DashboardScreen, ExamScreen
+from screens import LoginScreen, DashboardScreen, AdminScreen
+from exam import ExamsScreen
 
-class SM(ScreenManager):
-    pass
-
-class GoExam(App):
+class MyApp(App):
     def build(self):
-        sm = SM()
-
-        sm.auth = UserAuth()
-
-        sm.add_widget(LoginScreen(name="login"))
-        sm.add_widget(DashboardScreen(name="dashboard"))
-        sm.add_widget(ExamScreen(name="exam"))
-
+        self.auth = UserAuth()
+        
+        sm = ScreenManager()
+        sm.auth = self.auth
+        sm.lang = 'en'  # Afaan jalqabaa (Default language)
+        
+        # Wiijitoota screens adda addaa galchuu
+        sm.add_widget(LoginScreen(name='login'))
+        sm.add_widget(DashboardScreen(name='dashboard'))
+        sm.add_widget(AdminScreen(name='admin'))
+        sm.add_widget(ExamsScreen(name='exams'))
+        
         return sm
 
-GoExam().run()
+if __name__ == '__main__':
+    MyApp().run()
+    
