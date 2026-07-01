1ffa2a40102ce00e7374881090e3d8b5a5ec4d24
obsanurgi2011-boop <obsanurgi2011@gmail.com>
2026-07-01 18:23:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,9 @@
-# main.py
 from kivy.app import App
-from kivy.uix.screenmanager import ScreenManager, Screen
-from screens import MainScreen, ExamScreen
-from kivy.lang import Builder
+from kivy.uix.label import Label
 
-Builder.load_file('ui.kv')
-
-class MyScreenManager(ScreenManager):
-    pass
-
-class MainApp(App):
+class MyApp(App):
     def build(self):
-        sm = MyScreenManager()
-        sm.add_widget(MainScreen(name='main'))
-        sm.add_widget(ExamScreen(name='exam'))
-        return sm
+        return Label(text="Exam System Running")
 
-if __name__ == '__main__':
-    MainApp().run()
+if __name__ == "__main__":
+    MyApp().run()
